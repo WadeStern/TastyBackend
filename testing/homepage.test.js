cf7f2a750cfd0adb07d0a7e1919c900e1bff0a2f
WadeStern <wademstern@gmail.com>
2023-08-14 15:36:29 -0400

    s

--- testing/homepage.test.js
+++ testing/homepage.test.js
@@ -1,17 +1,24 @@
 const puppeteer = require("puppeteer");
+jest.setTimeout(60000);
 
 test("Get Title", async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage()
-  await page.goto('http://frontend.staging.wadestern.com/')
+  await page.goto('http://frontend.staging.wadestern.com/', {waitUntil: 'load', timeout: 20000})
   const title = await page.title()
   console.log(title)
   await browser.close()
 });
 test("Get Pythontest", async () => {
-  const browser = await puppeteer.launch({});
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
-  await page.goto("http://frontend.staging.wadestern.com/pythontest/");
+  await page.goto("http://frontend.staging.wadestern.com/pythontest/", {waitUntil: 'load', timeout: 20000});
   await page.waitForXPath('/html/body/div/div/div[2]');
   let [el] = await page.$x('/html/body/div/div/div[2]');
   const pythontest = await page.evaluate(name => name.innerText, el);
@@ -19,9 +26,12 @@ test("Get Pythontest", async () => {
   await browser.close();
 });
 test("Get 2nd title", async () => {
-  const browser = await puppeteer.launch({});
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
-  await page.goto("http://frontend.staging.wadestern.com/");
+  await page.goto("http://frontend.staging.wadestern.com/", {waitUntil: 'load', timeout: 20000});
   await page.waitForXPath('/html/body/div/div/div[2]/div[2]/div[2]/div/h3');
   let [el] = await page.$x('/html/body/div/div/div[2]/div[2]/div[2]/div/h3');
   const names = await page.evaluate(name => name.innerText, el);
