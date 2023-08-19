511bbf54cfa3b1282791cddb917abf1ac47d8211
WadeStern <wademstern@gmail.com>
2023-08-19 12:25:59 -0400

    s

--- testing/homepage.test.js
+++ testing/homepage.test.js
@@ -1,13 +1,15 @@
 const puppeteer = require("puppeteer");
 jest.setTimeout(60000);
 
+const prodorstaging = process.env.PRODORSTAGING;
+
 test("Get Title", async () => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox']
   });
   const page = await browser.newPage()
-  await page.goto('http://frontend.staging.wadestern.com/', {waitUntil: 'load', timeout: 20000})
+  await page.goto('http://frontend.${prodorstaging}.wadestern.com/', {waitUntil: 'load', timeout: 20000})
   const title = await page.title()
   console.log(title)
   await browser.close()
@@ -18,7 +20,7 @@ test("Get Pythontest", async () => {
     args: ['--no-sandbox']
   });
   const page = await browser.newPage();
-  await page.goto("http://frontend.staging.wadestern.com/pythontest/", {waitUntil: 'load', timeout: 20000});
+  await page.goto("http://frontend.${prodorstaging}.wadestern.com/pythontest/", {waitUntil: 'load', timeout: 20000});
   await page.waitForXPath('/html/body/div/div/div[2]');
   let [el] = await page.$x('/html/body/div/div/div[2]');
   const pythontest = await page.evaluate(name => name.innerText, el);
@@ -31,7 +33,7 @@ test("Get 2nd title", async () => {
     args: ['--no-sandbox']
   });
   const page = await browser.newPage();
-  await page.goto("http://frontend.staging.wadestern.com/", {waitUntil: 'load', timeout: 20000});
+  await page.goto("http://frontend.${prodorstaging}.wadestern.com/", {waitUntil: 'load', timeout: 20000});
   await page.waitForXPath('/html/body/div/div/div[2]/div[2]/div[2]/div/h3');
   let [el] = await page.$x('/html/body/div/div/div[2]/div[2]/div[2]/div/h3');
   const names = await page.evaluate(name => name.innerText, el);
